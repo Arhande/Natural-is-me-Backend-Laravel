345b357ac32f122bab06d24c02af4670c1f50755
AndrewAlvaroh <andrewalvaroh@gmail.com>
2021-10-17 22:25:50 +0700

    revisi frontend checkout

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -9,12 +9,15 @@
     <div class="lg:grid lg:grid-cols-3 gap-8">
           <div class="col-span-2">   
               
-              <div class="bg-gray-100 p-5 rounded-md">
-                  <h1 class="text-center md:text-left text-xl font-semibold">Detail Tagihan</h1> 
-                  <br /><br />
+              <div class="shadow-xl border  rounded-lg" style="background-color: #F1F5F9;">
+              <div class="rounded-t bg-white  px-6 py-6">
+                  <h6 class="text-blueGray-700 text-xl font-bold ">
+                    My account
+                  </h6>
+              </div>
                   
                   <form action="#">              
-                      <div class="md:flex flex-row md:space-x-4 w-full ">
+                      <div class="md:flex flex-row md:space-x-4 w-full px-10 mt-8">
                           <div class="mb-3  w-full ">
                               <label class="font-semibold text-gray-600 py-2">Nama Penerima <abbr title="required" class="text-red-600">*</abbr></label>
                               <input required="required" type="text" name="nama_penerima" id="nama_penerima" class="appearance-none block w-full  border rounded-lg h-10 px-4"  />
@@ -35,7 +38,7 @@
                             </div>
                         </div>
                         
-                        <div class="w-full mb-4">
+                        <div class="w-full mb-4 px-10">
                             <label class="font-semibold text-gray-600 py-2 block"> Provinsi <abbr title="required" class="text-red-600">*</abbr></label>
                             <input type="text" name="provinsi" id="#" class="w-full py-2 px-3">
                             @error('provinsi')
@@ -45,7 +48,7 @@
                             @enderror
                         </div>
                         
-                        <div class="w-full mb-4">
+                        <div class="w-full mb-4 mb-4 px-10">
                             <label class="font-semibold text-gray-600 py-2 block"> Kota / Kab<abbr title="required" class="text-red-600">*</abbr></label>
                             <input type="text" name="kota" id="#" class="w-full py-2 px-3/">
                             @error('kota')
@@ -55,7 +58,7 @@
                             @enderror
                         </div>
                         
-                        <div class="flex gap-6">
+                        <div class="flex gap-6 mb-4 px-10">
                             <div class="w-2/3 mb-4">
                                 <label class="font-semibold text-gray-600 py-2 block"> Kecamatan<abbr title="required" class="text-red-600">*</abbr></label>
                                 <input name="kecamatan" type="text" id="#" class="w-full py-2 px-3"/>
@@ -72,7 +75,7 @@
                 </div>
             </div>
             
-            <div class="flex-auto w-full mb-1 py-2">
+            <div class="flex-auto w-full mb-1 py-2 px-10">
                 <label class="font-semibold text-gray-600 py-2">Alamat</label>
                 <textarea  placeholder="Nama Jalan, nomor rumah Rt/Rw" spellCheck="false" defaultValue=""  required name="alamat"  class=" h-20 appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded-lg  py-4 px-4" >
                 </textarea>
@@ -89,14 +92,17 @@
 
 
 <!-- Pesanan anda -->
-<div class="col-span-1 bg-gray-100 rounded-md">      
-    
-    <div class="bg-gray-100 rounded-md">
-        <h1 class="text-center text-xl my-8 pt-8 md:pt-10 lg:pt-0 font-semibold">Pesanan Anda</h1>    
+<div class="col-span-1 mt-10 md:mt-16 lg:mt-0  rounded-md">        
+    <div class="shadow-xl border  rounded-lg" style="background-color: #F1F5F9;">
+            <div class="rounded-t bg-white  px-6 py-6">
+                <h6 class="text-blueGray-700 text-xl font-bold ">
+                     Pesanan Anda
+                </h6>
+            </div>
         <div class="mx-8">
             <div>
                 @foreach ($carts as $cart)
-                    <div class="flex justify-between my-2">
+                    <div class="flex justify-between my-2 mt-8">
                         <div> {{ $cart->product->first_name }} {{ $cart->product->last_name }}</div>
                         <div class="font-semibold"> {{ $cart->product->harga}} * {{ $cart->qty }} pcs</div>   
                     </div>
@@ -115,7 +121,7 @@
             <div class="my-5">Total  : <span class="font-semibold"> {{ $total }} </span></div>
         </div>
         
-        <div class="text-center mt-10 lg:mt-20">
+        <div class="text-center mt-10 lg:mt-20 pb-6">
             <button type="submit" class="border border-green-700 text-gray-100 rounded-md px-4 py-2 m-2 transition duration-500 ease select-none hover:text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:shadow-outline w-1/2" >
                 Lanjutkan Checkout
             </button>
@@ -124,8 +130,9 @@
         
     </div>
     
+    </div>
 </div>
-</div>
+
 </form>
 </div>
 <!-- End Pesanan anda -->
